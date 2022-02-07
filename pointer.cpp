87d196a85b2eda4f19f77363cdcb0dc9a03c00ed
9838827581 <98417928+9838827581@users.noreply.github.com>
2022-02-08 02:20:20 +0530

    interro of pointer

---
+++ pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int *p;
+    p=&a;
+    cout<<"HERE ARE SOME FUNCTION"<<endl;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p<<endl;
+}
